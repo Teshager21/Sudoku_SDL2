e77d87b8f0aad7f556db6fb0861ceeca216337dc
teshager <teshager8922@gmail.com>
2023-05-23 19:16:31 +0300

    add a cursor square to screen and read keyboard press

--- source.cpp
+++ source.cpp
@@ -10,7 +10,7 @@ int generateNumberofFilledCells();
 void generateFilledPositions(int(&filledPositions)[30], const int filledCells);
 void populateInitialCells(int(&tableArray)[3][3][3][3], const int filledCells, int filledPositions[]);
 bool isItRepeated(std::string scope, int scopeSpecifier, double value, int(&tableArray)[3][3][3][3]);
-
+void receiveInput(int(&tableArray)[3][3][3][3], int position, int value, std::string& messages);
 
 #define fps 20
 
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
     SDL_Window* window = NULL;
-    window = SDL_CreateWindow("SUDOKU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow("SUDOKU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         std::cout << "window not created! " << SDL_GetError();
     }
@@ -58,6 +58,31 @@ int main(int argc, char* argv[]) {
                 running = false;
                 break;
             }
+            if (SDL_KEYDOWN == event.type) {
+                std::cout << "key pressed down! "<<event.key.keysym.sym;
+                switch (event.key.keysym.sym) {
+                case SDLK_1: std::cout << "the number pressed is: 1";
+                    break;
+                case SDLK_2: std::cout << "the number pressed is: 2";
+                    break;
+                case SDLK_3: std::cout << "the number pressed is: 3";
+                    break;
+                case SDLK_4: std::cout << "the number pressed is: 4";
+                    break;
+                case SDLK_5: std::cout << "the number pressed is: 5";
+                    break;
+                case SDLK_6: std::cout << "the number pressed is: 6";
+                    break;
+                case SDLK_7: std::cout << "the number pressed is: 7";
+                    break;
+                case SDLK_8: std::cout << "the number pressed is: 8";
+                    break;
+                case SDLK_9: std::cout << "the number pressed is: 9";
+                    break;
+                case SDLK_LEFT: std::cout << "the number pressed is: LEFT";
+                    break;
+                }
+            }
         }
 
         capFrameRate(starting_tick);
@@ -66,29 +91,27 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
       
-        //Render grid
         //draw vertical lines
-        int gridSize = 30;
+        int gridSize = 50;
         for (int i = 0; i <= 9; i++) {
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-           SDL_RenderDrawLine(renderer, 50+gridSize, 50 + gridSize * (i + 1), 50+gridSize*10, 50 + gridSize * ((i + 1)));
+           SDL_RenderDrawLine(renderer, 20+gridSize, 20 + gridSize * (i + 1), 20+gridSize*10, 20 + gridSize * ((i + 1)));
         }
 
-    //draw horizontal lines
+       //draw horizontal lines
        for (int i = 0; i <= 9; i++) {
-          SDL_RenderDrawLine(renderer, 50 + gridSize * (i + 1), 50+gridSize, 50 + gridSize * (i + 1), 50+gridSize*10);  
+          SDL_RenderDrawLine(renderer, 20 + gridSize * (i + 1), 20+gridSize, 20 + gridSize * (i + 1), 20+gridSize*10);  
         }
 
      
-    TTF_Font* font = TTF_OpenFont("C:\\Windows\\Fonts\\arial.ttf", 20);
-    SDL_Color black = { 0,0,0,SDL_ALPHA_OPAQUE };
+    TTF_Font* font = TTF_OpenFont("C:\\Windows\\Fonts\\arial.ttf", 24);
+    SDL_Color black = { 0,0,255,SDL_ALPHA_OPAQUE };
     //create surface containing text
 
     SDL_Surface* surface = TTF_RenderText_Solid(font, "4", black);
     //create texture from the surface
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    //Render the texture screen
     //create a rectangle
     int hIndent = (gridSize - surface->w)*0.5;
     int vIndent = (gridSize - surface->h) * 0.5;
@@ -112,8 +135,10 @@ int main(int argc, char* argv[]) {
                     col = 3 * i + k;
                     row = 3 * j + l;
                     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-                    SDL_Rect rect = { 50 + gridSize * (col+1) + hIndent , 50 + gridSize * (row+1) + vIndent , surface->w,surface->h };
+                    SDL_Rect rect = { 20 + gridSize * (col+1) + hIndent , 20 + gridSize * (row+1) + vIndent , surface->w,surface->h };
+                    SDL_Rect cursor{ 20+gridSize,20+gridSize,gridSize,gridSize };
                     SDL_RenderCopy(renderer, texture, NULL, &rect);
+                    SDL_RenderCopy(renderer, texture, NULL, &cursor);
                     
 
                 }
@@ -123,6 +148,8 @@ int main(int argc, char* argv[]) {
         
         
     }
+    //a cursor square
+    SDL_Rect cursor { 30,30,50,50 };
     SDL_FreeSurface(surface);
     SDL_DestroyTexture(texture);
     TTF_CloseFont(font);
@@ -227,4 +254,28 @@ bool isItRepeated(std::string scope, int scopeSpecifier, double value, int(&tabl
         }
     }
     return false;
+}
+void receiveInput(int(&tableArray)[3][3][3][3], int position, int value, std::string& messages) {
+    int i, j, k, l;
+    l = position % 3;
+    if (position < 27) {
+        i = 0;
+        k = position / 9;
+    }
+    if (position > 26 && position < 54) {
+        i = 1;
+        k = (position - 27) / 9;
+    }
+    if (position > 53 && position < 81) {
+        i = 2;
+        k = (position - 54) / 9;
+    }
+    j = (position - 27 * i - 9 * k - 1) / 3;
+    if (isItRepeated("row", i * 3 + k, value, tableArray) || isItRepeated("col", j * 3 + l, value, tableArray) || isItRepeated("block", i * 3 + j, value, tableArray)) {
+        messages = "Repetition, please use another value!";
+    }
+    else {
+        tableArray[i][j][k][l] = value;
+        messages = "";
+    }
 }
